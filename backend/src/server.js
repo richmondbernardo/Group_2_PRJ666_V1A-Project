aa760cd2946830315339e473fda122f7223ff3d5
BaoVietHo <baovietho92@gmail.com>
2026-03-10 16:33:54 -0400

    removing console log for debug

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
-
 import express from "express";
 import { connectDB } from "./mongo.js";
 
